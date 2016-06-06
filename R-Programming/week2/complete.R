143b5b1f2c8808f88c983ea7b887a9adc9bb1060
mcdaidr1 <mcdaidr1@gmail.com>
2016-06-06 13:15:14 +0100

    Update complete.R

--- R-Programming/week2/complete.R
+++ R-Programming/week2/complete.R
@@ -1,12 +1,14 @@
 function(directory, id = 1:332){
+  # create numeric vector for nobs Data
   nobs = numeric()
+  # get files paths from directory
   files <- list.files(directory, full.names = TRUE)
   
   for (i in id) {
-    
+    # read file(s) from id
     readFile = read.csv(files[i])
     nobs = c(nobs, sum(complete.cases(readFile)))
   }
-  
+  # return mean Data frame 
   return(data.frame(id, nobs))
 }
